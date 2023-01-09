fe524480dc32656ac8cb5b0f2f3e391aacbffe12
MOHAMMAD SAIFUR RAHMAN <saifur.rahman@sslwireless.com>
2023-01-09 13:58:51 +0600

    routing not found issue fixing

--- src/routes/ErrorRoutes.js
+++ src/routes/ErrorRoutes.js
@@ -1,27 +1,24 @@
 import React from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 import NotFound from "pages/Error/NotFound"
-import {RouteLinks} from "constants/RouteLinks"
+import { RouteLinks } from "constants/RouteLinks"
 
 
 function ErrorRoutes() {
   const checkIfValidLinkExist = () => {
     let linkFound = 0;
-    for (const key in RouteLinks) 
-    {
-        if (window.location.pathname==RouteLinks[key]) 
-        {
-          linkFound = 1;
-        }
+    for (const key in RouteLinks) {
+      if (window.location.pathname == RouteLinks[key]) {
+        linkFound = 1;
+      }
     }
-
     return linkFound;
   }
 
   return (
     <Routes>
-      <Route path="*" element={ checkIfValidLinkExist() ? null : <Navigate to="/404" replace={true} /> } />
-      <Route path="/404" element={ <NotFound />} />
+      <Route path="*" element={checkIfValidLinkExist() ? null : <Navigate to="/404" replace={true} />} />
+      <Route path="/404" element={<NotFound />} />
     </Routes>
   )
 }
