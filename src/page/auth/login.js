d1cf42072ead068ec2718353c8770b497b21b4dc
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-01-11 20:18:31 +0600

    updated

--- src/page/auth/login.js
+++ src/page/auth/login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Col, Row, Space, Typography  } from 'antd';
+import { Card, Col, Row, Space, Typography } from 'antd';
 import { Button, Checkbox, Form, Input } from 'antd';
 
 const { Title } = Typography;
@@ -12,65 +12,65 @@ function Login() {
     console.log('Failed:', errorInfo);
   };
   return (
-    <Card 
-    title="Login"
+    <Card
+      title="Login"
     >
       <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      
-      <Form.Item
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
         wrapperCol={{
-          offset: 8,
           span: 16,
         }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Title className="text-success">Hello</Title>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-    </Form>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+
+        <Title className="text-success">Hello</Title>
+
+      </Form>
     </Card>
   );
 
