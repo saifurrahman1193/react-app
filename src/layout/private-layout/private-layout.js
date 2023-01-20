789a61525814f2a784e3f915a6f21b419e59c7cf
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-01-20 11:04:57 +0600

    structure change

--- src/layout/private-layout/private-layout.js
+++ src/layout/private-layout/private-layout.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import React, { lazy, useEffect } from 'react'
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
 import PrivateRoutes from 'route/private-routes'
 import Cookies from 'js-cookie';
 import AuthContextProvider from 'context/auth-context';
 import { Layout, Menu } from "antd";
+const LeftSidebar = lazy(()=>import('./left-sidebar'))
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -19,24 +20,27 @@ function PrivateLayout() {
 
     return (
         <AuthContextProvider>
-
-            <Layout>
-                <Header>Header</Header>
-                <Content>Content</Content>
-                <Footer>Footer</Footer>
+            <Layout className='private-layout'>
+                <Header>header</Header>
+                <Layout>
+                    <Sider>
+                        <LeftSidebar/>
+                    </Sider>
+                    <Content>
+                        <Outlet/>
+                        <Routes>
+                            {
+                                PrivateRoutes?.map((route, index) =>
+                                    <Route path={route?.path} element={<route.component />} key={'route-' + route?.name} />
+                                )
+                            }
+                        </Routes>
+                    </Content>
+                </Layout>
+                <Footer>footer</Footer>
             </Layout>
-
-
-            <Routes>
-                {
-                    PrivateRoutes?.map((route, index) =>
-                        <Route path={route?.path} element={<route.component />} key={'route-' + route?.name} />
-                    )
-                }
-            </Routes>
         </AuthContextProvider>
     )
 }
 
 export default PrivateLayout
-
