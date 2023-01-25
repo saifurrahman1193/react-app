59268d86dc7c41552ff177541121539614447b01
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-01-25 23:26:10 +0600

    header

--- src/layout/private-layout/top-bar.js
+++ src/layout/private-layout/top-bar.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
-import { MenuUnfoldOutlined, MenuFoldOutlined, UserOutlined } from '@ant-design/icons';
+import { MenuUnfoldOutlined, MenuFoldOutlined, UserOutlined, LogoutOutlined, ToolOutlined } from '@ant-design/icons';
 import { Layout } from "antd";
 import { PrivateLayoutContext } from 'context/private-layout-context'
 import { Avatar } from 'antd';
 import { Col, Row, Space } from 'antd';
 import { AuthContext } from 'context/auth-context'
+import { Dropdown, message } from 'antd';
+import { Link } from 'react-router-dom';
 
 const { Header } = Layout;
 
@@ -12,27 +14,53 @@ function TopBar() {
     const { leftSideBarCollapsed, setLeftSideBarCollapsed } = useContext(PrivateLayoutContext);
     const { user } = useContext(AuthContext);
 
+    const items = [
+        {
+            label: <Link to="/profile">Profile</Link>,
+            key: 'profile',
+            icon: <UserOutlined />,
+        },
+        {
+            label: <Link to="/change-password">Change Password</Link>,
+            key: 'change-password',
+            icon: <ToolOutlined />,
+        },
+        {
+            type: 'divider',
+        },
+        {
+            label: 'Logout',
+            key: 'logout',
+            icon: <LogoutOutlined />,
+        },
+    ];
+
 
     return (
         <Header>
             <Row>
-                <Col span={12}>
+                <Col span={1}>
                     {React.createElement(leftSideBarCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
                         className: 'trigger m-0 p-0',
                         onClick: () => setLeftSideBarCollapsed(!leftSideBarCollapsed),
                     })}
                 </Col>
-                <Col span={12}>
+                <Col span={23}>
                     <Row justify="end">
-                        <Space>
-                            <span>{user?.name}</span>
-                            <Avatar
-                                style={{
-                                    backgroundColor: '#87d068',
-                                }}
-                                icon={<UserOutlined />}
-                            />
-                        </Space>
+                        <Dropdown menu={{ items }}>
+                            <a onClick={(e) => e.preventDefault()} href='!#'>
+                                <Space>
+
+                                    <span>{user?.name}</span>
+                                    <Avatar
+                                        style={{
+                                            backgroundColor: '#87d068',
+                                        }}
+                                        icon={<UserOutlined />}
+                                    />
+                                </Space>
+                            </a>
+                        </Dropdown>
                     </Row>
                 </Col>
             </Row>
