0584b4f37119d154f7206d0519a103a9e2c5b80a
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-02-07 21:56:14 +0600

    Breadcrumb :

--- src/page/home.js
+++ src/page/home.js
@@ -1,10 +1,12 @@
 import Cookies from 'js-cookie';
 import React, { useEffect, useContext } from 'react'
 import { AuthContext } from 'context/auth-context'
+import { PrivateLayoutContext } from 'context/private-layout-context'
 
 
 function Home() {
     const { user, roles, permissions } = useContext(AuthContext);
+    const { setBreadcrumb } = useContext(PrivateLayoutContext);
 
     useEffect(() => {
         let token = Cookies.get("ProjectToken");
@@ -12,7 +14,14 @@ function Home() {
         if (!token) {
             window.location = process.env.REACT_APP_LOGIN_PAGE;
         }
+        setBreadcrumb(breadcrumb);
     }, []);
+
+    const breadcrumb = {
+        pageTitle: 'Home',
+        currentPath: '/',
+        layers: []
+    }
 
     return (
         <>
